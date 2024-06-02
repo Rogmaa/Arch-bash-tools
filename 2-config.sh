526d61fac2eec38eea1a5ba41f9715fbce861ad7
Rogmaa <143396904+Rogmaa@users.noreply.github.com>
2024-06-02 15:46:52 +0200

    Update 2-config.sh

--- 2-config.sh
+++ 2-config.sh
@@ -14,66 +14,45 @@ zoneinfo="Europe/Berlin"
 hostname="arch"
 username="rog"
 
-# ------------------------------------------------------
 # Set system time
-# ------------------------------------------------------
 ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
 hwclock --systohc
 
-# ------------------------------------------------------
 # Update reflector, adapt to your country of choice..
-# ------------------------------------------------------
 echo "Start reflector..."
 reflector -c "Germany," -p https -a 3 --sort rate --save /etc/pacman.d/mirrorlist
 
-# ------------------------------------------------------
 # Sync mirrors
-# ------------------------------------------------------
 pacman -Syy
 
-# ------------------------------------------------------
 # Install packages, feel free to adapt to your pref.
-# ------------------------------------------------------
-pacman --noconfirm -S grub xdg-desktop-portal-wlr efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack nash-completion openssh rsync reflector acpi acpi_call dnsmasq openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font exa bat htop ranger zip unzip neofetch duf xorg xorg-xinit xclip grub-btrfs xf86-video-amdgpu xf86-video-nouveau xf86-video-intel xf86-video-qxl brightnessctl pacman-contrib inxi
+pacman --noconfirm -S grub xdg-desktop-portal-wlr efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call dnsmasq openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font exa bat htop ranger zip unzip neofetch duf xorg xorg-xinit xclip grub-btrfs xf86-video-amdgpu xf86-video-nouveau xf86-video-intel xf86-video-qxl brightnessctl pacman-contrib inxi
 
-# ------------------------------------------------------
 # set lang utf8 US
-# ------------------------------------------------------
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-# ------------------------------------------------------
 # set keyboard
-# ------------------------------------------------------
 echo "FONT=ter-v18n" >> /etc/vconsole.conf
 echo "KEYMAP=$keyboardlayout" >> /etc/vconsole.conf
 
-# ------------------------------------------------------
 # set hostname and localhost
-# ------------------------------------------------------
 echo "$hostname" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.0.1 $hostname.localdomain $hostname" >> /etc/hosts
 clear
-
-# ------------------------------------------------------
 # set root pwd
-# ------------------------------------------------------
 echo "Set root password"
 passwd root
 
-# ------------------------------------------------------
 # Add your user
-# ------------------------------------------------------
 echo "Add user $username"
 useradd -m -G wheel $username
 passwd $username
 
-# ------------------------------------------------------
 # Enable services.. add and remove services if neeed
-# ------------------------------------------------------
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
@@ -84,31 +63,23 @@ systemctl enable fstrim.timer
 systemctl enable firewalld
 systemctl enable acpid
 
-# ------------------------------------------------------
 # Grub install, adapt if you prefer another bootloader
-# ------------------------------------------------------
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# ------------------------------------------------------
 # Add btrfs and setfont to mkinitcpio, feel free to adapt.
-# ------------------------------------------------------
 sed -i 's/BINARIES=()/BINARIES=(btrfs setfont)/g' /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
-# ------------------------------------------------------
 # Add usr to wheel
-# ------------------------------------------------------
 clear
 echo "Uncomment %wheel group in sudoers around line 85:"
 echo ""
-read -p "Open sudoers now?" c
+read -p "Open sudoers now.." c
 EDITOR=vim sudo -E visudo
 usermod -aG wheel $username
 
-# ------------------------------------------------------
 # Copy scripts to home dir
-# ------------------------------------------------------
 cp /archinstall/3-yay.sh /home/$username
 cp /archinstall/4-zram.sh /home/$username
 cp /archinstall/5-timeshift.sh /home/$username
